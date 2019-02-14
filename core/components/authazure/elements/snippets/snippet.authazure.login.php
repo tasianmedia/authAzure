eac987ca92f95cc59af24ec5686b87b8d4d422cf
David Pede <davidpede@users.noreply.github.com>
2019-02-14 14:42:47 +0000

    Fetch full ms account logout address

--- core/components/authazure/elements/snippets/snippet.authazure.login.php
+++ core/components/authazure/elements/snippets/snippet.authazure.login.php
@@ -20,6 +20,7 @@ $logoutTpl = !empty($logoutTpl) ? $logoutTpl : '';
 $output = '';
 $phs = array();
 
+//TODO add sys setting to make optional
 $modx->regClientStartupHTMLBlock('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">');
 $modx->regClientHTMLBlock('<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" integrity="sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB" crossorigin="anonymous"></script>');
 
@@ -34,7 +35,7 @@ if (!$modx->user->isAuthenticated($modx->context->key)) {
     $output = $modx->getChunk($loginTpl, $phs);
 } else {
     $phs['aaz']['logout_url'] = $authazure->getActionUrl('logout');
-    $phs['aaz']['logout_azure_url'] = $authazure->getActionUrl('logout'); //TODO need func to fetch full logout url
+    $phs['aaz']['logout_azure_url'] = $authazure->getLogoutUrl();
     $output = $modx->getChunk($logoutTpl, $phs);
 }
 
